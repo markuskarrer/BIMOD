77d88978bfc5ec0ce832b30a89f7007cfffe3162
Markus Karrer <markus.karrer@uni-koeln.de>
2020-04-02 08:49:02 +0200

    clean up loops for namelist

--- McSnow_Pamtra_quicklooks_bimodalityexp.sh
+++ McSnow_Pamtra_quicklooks_bimodalityexp.sh
@@ -47,122 +47,111 @@ fi
 ###
 #use next line to plot McSnow runs with different geometries
 ###
-#export McSnow_testcase=("bimodal1mode" "bimodal2mode") #  "bimodal2mode") # "bimodal_nucleation")
-export McSnow_testcase=("bimodal2mode") #  "bimodal2mode") # "bimodal_nucleation")
+export McSnow_testcase=("bimodal2mode") 
 
 #switch of processes (for McSnow this is so far not used)
 export switch_off_processes_list=("_") # "_noicesnow_nosnowself" "_nosnowself" "_noicesnow") # _noiceself, _noicesnow, _nosnowself
 
 #something just needed for SB
-export model_setup_specifier=("_") #dont mess with SB settings if you are not running SB
+export model_setup="_" #dont mess with SB settings if you are not running SB
 export model_setup_specifier_onestring="_"
 for testcase in "${McSnow_testcase[@]}"
 do
     #loop over different namelist settings (relevant for running McSnow not for plotting)
-    ssat_array=(0) # 0 0) #10 20 30 40 50)  #[1/1000] -> 1 is 0.001
-    stick_array=(2) # 2 2) #2 2 2 2 2 3 3 3 3 3) # 0: E_s=1, 1: PK97, 2: Connolly12, 3: 0.5*Connolly12
-    ncl_array=(50) #  100 200) #
-    #nclmass_array=(1 1 1 5 5 0 10) #
-    nclmass_array=(100) # 100 100) #
-    McSnow_geom_array=(2) # 2 2 )  # 1.constant 2.binary 3.monodep
+    ssat_array=(0) #(0 10 20 30 40 50)  #supersaturation over ice: [1/1000] -> 1 is 0.001
+    stick_array=(2) #sticking efficiency: 0: E_s=1, 1: PK97, 2: Connolly12, 3: 0.5*Connolly12
+    ncl_array=(0) #(10 100 200) #nucleation rate [3/100 SP/sm3] #setting a high numer gets expensive (CPU-time and memory!); compensate this by a high multiplicity (xi0 in runscript: McSnow_runscripts/1d_bimodal2mode)
+    nclmass_array=(1000) #( 100 100) #
+    McSnow_geom=2 # 1.constant 2.binary 3.monodep #we need binary here
     len_namelistcomb=${#ssat_array[@]}
-    for (( i_namelistcomb = 0 ; i_namelistcomb < ${#ssat_array[@]} ; i_namelistcomb++ ))
-    do
+    for (( i_ssat = 0 ; i_ssat < ${#ssat_array[@]} ; i_ssat++ )); do
+        ssat=${ssat_array[$i_ssat]}
+        for (( i_stick = 0 ; i_stick < ${#stick_array[@]} ; i_stick++ )); do
+            stick=${stick_array[$i_stick]}
+            for (( i_ncl = 0 ; i_ncl < ${#ncl_array[@]} ; i_ncl++ )); do
+                ncl=${ncl_array[$i_ncl]}
+                for (( i_nclmass = 0 ; i_nclmass < ${#nclmass_array[@]} ; i_nclmass++ )); do
+                    nclmass=${nclmass_array[$i_nclmass]}
+                #bash doesnt care about indentation; for looping over other variables just yank the for loop and the following line
 
-        ssat=${ssat_array[$i_namelistcomb]}
-        stick=${stick_array[$i_namelistcomb]}
-        McSnow_geom=${McSnow_geom_array[$i_namelistcomb]}
-        ncl=${ncl_array[$i_namelistcomb]}
-        nclmass=${nclmass_array[$i_namelistcomb]}
         
-        echo "#################"
-        echo "analyzing testcase: "$testcase "with ssat=" $ssat ", stick=" $stick
-        echo "#################"
-        export testcase;
-echo $testcase 
+    echo "#################"
+    echo "analyzing testcase: "$testcase "with ssat=" $ssat ", stick=" $stick ",ncl=," $ncl ",nclmass," $nclmass
+    echo "#################"
+    export testcase; export McSnow_geom; export model_setup; export switch_off_processes #make them visible for called routines
+            
+    export tstep="300" #start of timestep analyzed by PAMTRA and plotting routine in minutes
+    export tstep_end="600" #end timestep analyzed by PAMTRA and plotting routine in minutes (defines end of averaging period)
+    export av_tstep="5" #average the McSnow output in the PAMTRA adaption by ... s #do not set this lower than dtc (f.e. "5"s)
+
+    if [[ "$execwhat" == *r1* ]] ; then #recompile for a change in the model
+        echo "############"
+        echo "recompiling"
+        echo "############"
+
+        cd $MC
+
+        #alternative if cheops is shut down
+        make release_omp #alternative if cheops is shut down
+    fi
+    #get foldername from current runscript (even when it is not executed
+    cd $BIMOD
+    cp McSnow_runscripts/1d_${testcase%_*} $MC/run/1d
+    export experiment=$($MC/run/1d "onlyname" $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto  $ncl $nclmass)
+    echo "analyze experiment:" $experiment
+    
+    if [[ "$execwhat" == *Mc1* ]] ; then #run McSnow (f.e 1d-model)
+        echo "############"
+        echo "run McSnow"
+        echo "############"
+
+        cd $MC/run #alternative if cheops is shut down
+        echo "SETTINGS: " $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto $testcase $habit $ncl $nclmass
+        ./1d "fullrun" $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto  $ncl $nclmass
+    fi
+
+    if [[ "$execwhat" == *ncdf1* ]] ; then #run McSnow_dat2ncdf.py i.e. produce a netCDF in the right time averaging step
+    #convert the mass2fr...dat to ncdf and derive temporal averages
+        cd $BIMOD
+        echo "converting McSnowoutput from .dat to .ncdf"
+        python McSnow_dat2ncdf.py
+    fi
+
+    #switch between different versions of adaption #this is not in the next if clause because it is also needed in the plotting routines
+    export adapt_version=3 #1-> histogram as input 2-> own category for each SP 3-> KDE as input
+
+    if [[ "$execwhat" == *Pam1* ]] ; then #let PAMTRA run on output
+        echo "############"
+        echo "run Pamtra"
+        echo "############"
+
+        cd /home/mkarrer/Dokumente/pamtra/ #TODO: REPLACE by <PAM_path>
+        #run PAMTRA
+        python McSnow_adapt_bimodal.py
+    fi
+     if [[ "$execwhat" == *pl1* ]] ; then #produce quicklook of McSnow and the corresponding PAMTRA run
+        echo "############"
+        echo "plotting"
+        echo "############"
+
+        cd $BIMOD
+
+        #the next two lines I used for plotting McSnow model output  (if you need something like this just ask me, they are not clean, but we might get them running)
+        #python overview_panel.py 
+        #python plot_fluxes_and_densities.py
         
-        
-        for switch_off_processes in "${switch_off_processes_list[@]}"
-        do
-        echo  "${model_setup_specifier[@]}"
-            for model_setup in "${model_setup_specifier[@]}"
-            do
-                echo "run " $testcase " with McSnow geometry: " $McSnow_geom ", SB geometry: " $model_setup ", with deactivated processes: " $switch_off_processes
-                export McSnow_geom; export model_setup; export switch_off_processes #make them visible for called routines
-                
-                #modify the MC src code
-                #./modify_MC_src.sh  $habit
-                export tstep="300" #timestep analyzed by PAMTRA and plotting routine in minutes
-                export tstep_end="600" #"600" #timestep analyzed by PAMTRA and plotting routine in minutes (defines end of averaging period)
-                export av_tstep="5" #average the McSnow output in the PAMTRA adaption by ... s #do not set this lower than dtc (f.e. "5"s)
+        export plot_totalice="False"
+        python bimodalitystudy_DWRs.py
+        python plot_spectra_and_moments.py
 
-                if [[ "$execwhat" == *r1* ]] ; then #recompile for a change in the model
-                    echo "############"
-                    echo "recompiling"
-                    echo "############"
+    fi                 
 
-                    cd $MC
 
-                    #alternative if cheops is shut down
-                    make release_omp #alternative if cheops is shut down
-                fi
-                #get foldername from current runscript (even when it is not executed
-                cd $BIMOD
-                cp McSnow_runscripts/1d_${testcase%_*} $MC/run/1d
-                export experiment=$($MC/run/1d "onlyname" $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto  $ncl $nclmass)
-                echo "analyze experiment:" $experiment
-                
-                if [[ "$execwhat" == *Mc1* ]] ; then #run McSnow (f.e 1d-model)
-                    echo "############"
-                    echo "run McSnow"
-                    echo "############"
-
-                    cd $MC/run #alternative if cheops is shut down
-                    echo "SETTINGS: " $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto $testcase $habit $ncl $nclmass
-                    ./1d "fullrun" $ssat $stick $model_setup $McSnow_geom $depos_from0mto $riming_fromdeposto  $ncl $nclmass
-                fi
-
-                if [[ "$execwhat" == *ncdf1* ]] ; then #run McSnow_dat2ncdf.py i.e. produce a netCDF in the right time averaging step
-                #convert the mass2fr...dat to ncdf and derive temporal averages
-                    cd $BIMOD
-                    echo "converting McSnowoutput from .dat to .ncdf"
-                    python McSnow_dat2ncdf.py
-                fi
-
-                #switch between different versions of adaption #this is not in the next if clause because it is also needed in the plotting routines
-                export adapt_version=3 #1-> histogram as input 2-> own category for each SP 3-> KDE as input
-
-                if [[ "$execwhat" == *Pam1* ]] ; then #let PAMTRA run on output
-                    echo "############"
-                    echo "run Pamtra"
-                    echo "############"
-
-                    cd /home/mkarrer/Dokumente/pamtra/ #TODO: REPLACE by <PAM_path>
-                    #run PAMTRA
-                    python McSnow_adapt_bimodal.py
-                fi
-                 if [[ "$execwhat" == *pl1* ]] ; then #produce quicklook of McSnow and the corresponding PAMTRA run
-                    echo "############"
-                    echo "plotting"
-                    echo "############"
-
-                    cd $BIMOD
-
-                    #the next two lines I used for plotting McSnow model output  (if you need something like this just ask me, they are not clean, but we might get them running)
-                    #python overview_panel.py 
-                    #python plot_fluxes_and_densities.py
-                    
-                    export plot_totalice="False"
-                    python bimodalitystudy_DWRs.py
-                    python plot_spectra_and_moments.py
-
-                fi                 
-
-                done #for model_setup in "${model_setup_specifier[@]}"
-            done #for switch_off_processes in ...
-         done #for habit in "${habits[@]}"
-  
-    done #for namelistparams in "{1..${#distro[@]}}"
+    done #for ssat
+    done #for stick
+    done #for ncl
+    done #for nclmass
+done #for testcase
 
 
 cd $BIMOD #go back to start directory
